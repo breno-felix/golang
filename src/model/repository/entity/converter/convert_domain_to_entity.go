5587dc76200acd40ab5a55ac501f8abc95d23d7a
Breno Felix <brenodev.felix@gmail.com>
2025-03-08 14:43:22 -0300

    :sparkles: feat: Add ConvertDomainToEntity function for transforming UserDomain to UserEntity

---
+++ src/model/repository/entity/converter/convert_domain_to_entity.go
@@ -0,0 +1,17 @@
+package converter
+
+import (
+	"github.com/breno-felix/golang/src/model"
+	"github.com/breno-felix/golang/src/model/repository/entity"
+)
+
+func ConvertDomainToEntity(
+	domain model.UserDomainInterface,
+) *entity.UserEntity {
+	return &entity.UserEntity{
+		Email:    domain.GetEmail(),
+		Password: domain.GetPassword(),
+		Name:     domain.GetName(),
+		Age:      domain.GetAge(),
+	}
+}
