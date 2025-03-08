4efdc70aa6e18c8a3f2ca6348e1314ffa8eacb6a
Breno Felix <brenodev.felix@gmail.com>
2025-03-08 15:08:02 -0300

    :sparkles: feat: Initialize server and set up MongoDB connection in main.go

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
-	"os"
 
+	"github.com/breno-felix/golang/src/configuration/database/mongodb"
+	"github.com/breno-felix/golang/src/configuration/logger"
+	"github.com/breno-felix/golang/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Starting server...")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println(os.Getenv("TEST"))
+
+	database, err := mongodb.NewMongoDBConnection(context.Background())
+	if err != nil {
+		log.Fatalf(
+			"Error try to connect to database, error=%s \n",
+			err.Error())
+		return
+	}
+
+	userController := initDependencies(database)
+
+	router := gin.Default()
+
+	routes.InitRoutes(router, userController)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
