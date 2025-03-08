be78967e5ba16b0b68cc55e5545672920b2c523d
Breno Felix <brenodev.felix@gmail.com>
2025-03-08 14:35:38 -0300

    :sparkles: feat: Add UserDomainInterface and user domain creation functions

---
+++ src/model/user_domain_interface.go
@@ -0,0 +1,35 @@
+package model
+
+type UserDomainInterface interface {
+	GetEmail() string
+	GetPassword() string
+	GetAge() int8
+	GetName() string
+	GetId() string
+
+	SetId(string)
+
+	EncryptPassword()
+}
+
+func NewUserDomain(
+	email, password, name string,
+	age int8,
+) UserDomainInterface {
+	return &userDomain{
+		email:    email,
+		password: password,
+		name:     name,
+		age:      age,
+	}
+}
+
+func NewUserUpdateDomain(
+	name string,
+	age int8,
+) UserDomainInterface {
+	return &userDomain{
+		name: name,
+		age:  age,
+	}
+}
